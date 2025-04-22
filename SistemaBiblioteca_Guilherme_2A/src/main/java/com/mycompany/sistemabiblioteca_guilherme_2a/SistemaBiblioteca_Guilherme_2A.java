f9ebef74c722fcc6fa617bd7509a3234e959a28d
Gulermehub <silvaguilhermerlm@gmail.com>
2025-04-22 12:26:45 -0300

    Add files via upload

--- SistemaBiblioteca_Guilherme_2A/src/main/java/com/mycompany/sistemabiblioteca_guilherme_2a/SistemaBiblioteca_Guilherme_2A.java
+++ SistemaBiblioteca_Guilherme_2A/src/main/java/com/mycompany/sistemabiblioteca_guilherme_2a/SistemaBiblioteca_Guilherme_2A.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.sistemabiblioteca_guilherme_2a;
 
-/**
- *
- * @author 2024105070039
- */
 public class SistemaBiblioteca_Guilherme_2A {
 
     public static void main(String[] args) {
