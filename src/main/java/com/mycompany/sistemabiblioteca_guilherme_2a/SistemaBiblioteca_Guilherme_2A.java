9223e7fb97a92a2d3a594ab00bf1b08d0257e05f
silva <silva@GUILHERME>
2025-05-11 17:03:45 -0300

    Usuario, Administrador e Biblioteca feitos, faltando apenas o main

--- src/main/java/com/mycompany/sistemabiblioteca_guilherme_2a/SistemaBiblioteca_Guilherme_2A.java
+++ src/main/java/com/mycompany/sistemabiblioteca_guilherme_2a/SistemaBiblioteca_Guilherme_2A.java
@@ -1,8 +1,65 @@
 package com.mycompany.sistemabiblioteca_guilherme_2a;
 
-public class SistemaBiblioteca_Guilherme_2A {
+import java.util.Scanner;
 
+public class SistemaBiblioteca_Guilherme_2A {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+    
+    //chamando a classe Biblioteca
+        Biblioteca biblioteca = new Biblioteca();
+        Scanner scanner = new Scanner(System.in);
+    //iniciamos um loop while pra roda o programa
+    
+    while (true){
+    //menu de opções
+            System.out.println("======= Sistema de Biblioteca ===========");
+            System.out.println("=======1. Cadastrar Livro ========");
+            System.out.println("=======2. Cadastrar Usuário=========");
+            System.out.println("====3. Listar Livros=======");
+            System.out.println("========4. Emprestar Livro=======");
+            System.out.println("======5. Sair======");
+            System.out.print("Escolha uma opção: ");
+            int op = scanner.nextInt();
     }
+    //switch para cada opção
+      
+        
+        
+        
+        }
+    }
+        
+     
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    }
+       
+        
+     
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    }
+   
 }
